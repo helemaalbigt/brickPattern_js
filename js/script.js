613da725d8e2a1ac048a1ec4bff7b624bd759bae
Thomas <van.bouwel.thomas@gmail.com>
2014-10-24 00:54:41 +0200

    added: reads parameters but draws very slowly

--- js/script.js
+++ js/script.js
@@ -7,36 +7,31 @@
 var scaleFactor = 1;
 //the factor by which we multiply to go from real dimension to pixel dimension
 //Facade
-var fw = 500;
-//real facade width in cm
-var fh = 1200;
-//real facade height in cm
+var fw = 900;//real facade width in cm
+var fh = 600;//real facade height in cm
 //Bricks
 var bricks = new Array();
-var bw = 32;
-//real width of brick in cm
-var bh = 16;
-//real heigth of brick in cm
-var pxW = 32;
-//brick absolute width in pixels
-var pxH = 16;
-//brick absolute height in pixels
+var bw = 32;//real width of brick in cm
+var bh = 16;//real heigth of brick in cm
+var pxW = 32;//brick absolute width in pixels
+var pxH = 16;//brick absolute height in pixels
 //Colors
-var brickColors = new Array();
-//array containing all possible brick colors
+var brickColors = new Array();//array containing all possible brick colors
 var c1 = new fabric.Color('rgb(10,10,10)');
 var c2 = new fabric.Color('rgb(30,30,30)');
 var c3 = new fabric.Color('rgb(50,50,50)');
 var c4 = new fabric.Color('rgb(70,70,70)');
 var c5 = new fabric.Color('rgb(90,90,90)');
-var c6;
+var c6 = new fabric.Color('rgb(100,0,0)');
 //Joints
-var jointThickness = 1;
-//real joint thickness in cm
+var jointThickness = 1;//real joint thickness in cm
 var jc = new fabric.Color('rgb(160,160,160)');
+//Draw
+var drawmode = "uniform";
 
 //Canvas
 var canvas;
+var ctx;
 
 //do everything that needs to be done when document loads
 $(document).ready(function() {
@@ -53,7 +48,7 @@ $(document).ready(function() {
 			$(el).colpickHide();
 		}
 	}).css('background-color', '#ff8800');
-	
+
 	/*
 	 * initialize fabric canvas
 	 */
@@ -62,17 +57,41 @@ $(document).ready(function() {
 	canvas.setHeight($("#canvas_wrapper").height());
 	//disable renderOnAddRemove to increase performance
 	canvas.renderOnAddRemove = false;
-	
-	c6 = new fabric.Color('rgb(200,0,200)');
+
+	/*
+	 * add eventlistener to all parameters and update them
+	 */
+	$( ".input" ).change(function() {
+  		updateParameters();
+	});
+
 });
 
 function refresh() {
-	var b = new Brick(0, 0, bw, bh, c6);
-	b.draw();
+	canvas.clear().renderAll();
+	updateParameters();
+	for(var w =0; w<fw; w+=bw){
+		for(var h =0; h<fh; h+=bh){
+			//console.log(h+" "+bh+" "+fh);
+			var b = new Brick(w, h, bw, bh, c6);
+			b.draw();
+		}
+	}
+	
 }
 
 /**
- * CLASS brick 
+ *Updates all parameter 
+ */
+function updateParameters(){
+	fw = parseInt($('#fw').val());
+	fh = parseInt($('#fh').val());
+	bw = parseInt($('#bw').val());
+	bh = parseInt($('#bh').val());
+}
+
+/**
+ * CLASS brick
  *
  * @param float
  * @param float
@@ -95,34 +114,21 @@ function Brick(xPos, yPos, brickW, brickH, c) {
  * @return
  */
 Brick.prototype.draw = function() {
-	 var rect = new fabric.Rect({
-		 left : this.xPos,
-		 top : this.yPos,
-		 fill : 'rgb(200,0,200)',
-		 width : this.brickW,
-		 height : this.brickH,
-		 stroke : jc,
-		 strokeWidth : 1,
-		 hasControls : false,
-		hasRotatingPoint : false,
-		hasBorders : false,
-		evented : false
-	 });
 
-	 canvas.add(rect);
-	 canvas.renderAll();
-
-	/*var circle = new fabric.Circle({
-		radius : 20,
-		fill : 'green',
-		left : 100,
-		top : 100,
+	var rect = new fabric.Rect({
+		left : this.xPos,
+		top : this.yPos,
+		fill : 'rgb(200,0,200)',
+		width : this.brickW,
+		height : this.brickH,
+		stroke : jc,
+		strokeWidth : 1,
 		hasControls : false,
 		hasRotatingPoint : false,
 		hasBorders : false,
 		evented : false
 	});
 
-	canvas.add(circle);
-	canvas.renderAll();*/
+	canvas.add(rect);
+	canvas.renderAll();
 }
