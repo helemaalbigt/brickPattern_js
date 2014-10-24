3614679dfd049fa76c38bb91146054f326632867
Thomas <van.bouwel.thomas@gmail.com>
2014-10-24 18:00:36 +0200

    added: working scaling, random, colorpicking

--- js/script.js
+++ js/script.js
@@ -18,7 +18,13 @@ var pxH = 16;//brick absolute height in pixels
 var offset = 0;//offset each row of bricks should have relative to the previous one
 //Colors
 var brickColors = new Array();//array containing all possible brick colors
-
+var nc = 6;
+var c1 = "#c95d38";
+var c2 = "#c95d38";
+var c3 = "#c95d38";
+var c4 = "#c95d38";
+var c5 = "#c95d38";
+var c6 = "#c95d38";
 //Joints
 var jointThickness = 1;//real joint thickness in cm
 //Draw
@@ -32,17 +38,33 @@ var ctx;
 $(document).ready(function() {
 
 	/*
+	 * append hidden input with hex value behind brickcolor boxes
+	 */
+	var colorboxcounter = 1;
+	$('.color-box').each(function() {
+    	$(this).after("<input id=\"c"+colorboxcounter+"\" type=\"hidden\" value=\"#c95d38\"></input>");
+		colorboxcounter++;
+	});
+	
+	/*
 	 * create the brickcolor boxes
 	 */
 	$('.color-box').colpick({
 		colorScheme : 'dark',
 		layout : 'rgbhex',
-		color : 'ff8800',
+		color : '#c95d38',
 		onSubmit : function(hsb, hex, rgb, el) {
 			$(el).css('background-color', '#' + hex);
 			$(el).colpickHide();
+			$(el).next().attr("value",'#' + hex);
+			updateParameters();
 		}
-	}).css('background-color', '#ff8800');
+	}).css('background-color', '#c95d38');
+	
+	/*
+	 * Put all colors in array
+	 */
+	brickColors = [c1,c2,c3,c4,c5,c6];
 
 	/*
 	 * initialize canvas
@@ -64,27 +86,46 @@ $(document).ready(function() {
 
 });
 
+
+/**
+ *Main Refresh function, used to generate and draw the bricks 
+ */
 function refresh() {
 	//clear canvas
 	canvas.width = canvas.width;
 	//update all inputparameter
 	updateParameters();
+	//resize canvas to facade size
+	$("#canvas").attr("width",fw);
+	$("#canvas").attr("height",fh);
+	
+	//rescale canvas to screen size
+	console.log(fh+"-"+$("#canvas_wrapper").height());
+	//if one of the borders crosses the screen, resize
+	if(fw>$("#canvas_wrapper").width() || fh>$("#canvas_wrapper").height()){
+		//determine scalefactor
+		scaleFactor = Math.min($("#canvas_wrapper").width()/fw, $("#canvas_wrapper").height()/fh);
+
+		//correctcanvas border
+		$("#canvas").attr("width",fw*scaleFactor);
+	} else{
+		scaleFactor = 1;
+	} 
+	ctx.scale(scaleFactor,scaleFactor);
+		
+	//Calculate brick positions
 	//calculate the offset to be used
 	var os = offset%bw;
 	var osTotal = 0;
 	//generate all the bricks
 	for(var h =0; h<fh; h+=bh){
-		for(var w =0; w<fw; w+=bw){
+		for(var w =0; w<fw+bw; w+=bw){
 			//console.log(h+" "+bh+" "+fh);
-			var b = new Brick(w + osTotal, h, bw, bh, "rgb(200,100,60)");
+			var b = new Brick(w - osTotal, h, bw, bh, pickColor());
 			b.draw();
 		}
-		//handle offset Total
-		if(osTotal+os<(bw)){
-			osTotal+=os;
-		}  else{
-			osTotal = 0;
-		}
+		//handle offset Total counter
+		osTotal = (osTotal+os<(bw)) ? (osTotal+os) : 0;
 	}
 	
 }
@@ -98,6 +139,36 @@ function updateParameters(){
 	bw = parseInt($('#bw').val());
 	bh = parseInt($('#bh').val());
 	offset = parseInt($('#offset').val());
+	
+	nc = parseInt($('#nc').val());
+	brickColors[0] = $('#c1').val();
+	brickColors[1] = $('#c2').val();
+	brickColors[2] = $('#c3').val();
+	brickColors[3] = $('#c4').val();
+	brickColors[4] = $('#c5').val();
+	brickColors[5] = $('#c6').val();
+	
+	drawmode = $('#drawmode').val();
+}
+
+/**
+ *Updates all parameter 
+ * 
+ * @return String Color in hex format
+ */
+function pickColor(){
+	var returnString = "";
+	
+	switch (drawmode) { 
+	    case 'random': 
+	        returnString = brickColors[Math.floor(Math.random() * (nc))];
+	        break;
+	    default:
+	        returnString = brickColors[0];
+	        break;
+	}
+	
+	return returnString;
 }
 
 /**
@@ -124,6 +195,7 @@ function Brick(xPos, yPos, brickW, brickH, c) {
  * @return
  */
 Brick.prototype.draw = function() {
+	console.log(this.c);
 	ctx.beginPath();
     ctx.rect(this.xPos,this.yPos,this.brickW,this.brickH);
     ctx.fillStyle = this.c;
